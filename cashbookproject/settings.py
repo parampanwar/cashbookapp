dbd8278ce698063e7cf5ebafbd92d5b6f267348e
Param Panwar <85060442+parampanwar@users.noreply.github.com>
2024-10-07 15:33:12 +0530

    Update settings.py

--- cashbookproject/settings.py
+++ cashbookproject/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'cashbookproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+        'NAME': BASE_DIR / 'db.sqlite3',  # Ensure this path is correct
+    }
 }
 
 
